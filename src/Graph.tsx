946de01b633002bcefae12eaa71b1f6209d539fe
Sarthakkathuria <sarthakkathuria2001@gmail.com>
2020-04-27 01:11:15 +0530

    Graph fixed

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -32,7 +32,7 @@ class Graph extends Component<IProps, {}> {
 
   componentDidMount() {
     // Get element to attach the table from the DOM.
-    const ele = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+    const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
     const schema = {
       stock: 'string',
