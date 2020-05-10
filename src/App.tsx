988f5a8e8dd03835e29c0ad900b6d17693729ffb
AyushShri <shrivastavayush32@gmail.com>
2020-05-11 01:19:11 +0530

    Created Graph based Stock Dashboard for JMC Pespective tool and added patch file

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import DataStreamer, { ServerRespond } from './DataStreamer';
 import Graph from './Graph';
 import './App.css';
+import { ServerResponse } from 'http';
 
 /**
  * State declaration for <App />
  */
 interface IState {
   data: ServerRespond[],
+  showgraph:boolean,
 }
 
 /**
@@ -22,6 +24,7 @@ class App extends Component<{}, IState> {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
+      showgraph:false,
     };
   }
 
@@ -29,19 +32,30 @@ class App extends Component<{}, IState> {
    * Render Graph react component with state.data parse as property data
    */
   renderGraph() {
+    if(this.state.showgraph){
     return (<Graph data={this.state.data}/>)
+    }
   }
 
   /**
    * Get new data from server and update the state with the new data
    */
   getDataFromServer() {
-    DataStreamer.getData((serverResponds: ServerRespond[]) => {
-      // Update the state by creating a new array of data that consists of
-      // Previous data in the state and the new data from server
-      this.setState({ data: [...this.state.data, ...serverResponds] });
+    let c=0;
+    const interval=setInterval(() => {
+      DataStreamer.getData((serverResponds: ServerRespond[]) => {
+      this.setState({
+        data:   serverResponds,
+        showgraph:true,
+      } );
     });
-  }
+    c++;
+    if(c>1000){
+      clearInterval(interval);
+    } 
+  }, 100);
+}
+    
 
   /**
    * Render the App react component
