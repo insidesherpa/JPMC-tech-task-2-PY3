3bc997f206f78636c7b22bbe52d1c103d6119aeb
jcrawfo4 <jkbcrawford@gmail.com>
2022-10-31 15:03:11 -0500

    graph stock output works

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -32,7 +32,7 @@ class Graph extends Component<IProps, {}> {
 
   componentDidMount() {
     // Get element to attach the table from the DOM.
-    const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+    const elem = document.getElementsByTagName('perspective-viewer')[0] as any as PerspectiveViewerElement;
 
     const schema = {
       stock: 'string',
